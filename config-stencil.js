e73981a9f6738a4ba2c59f3d0d1c4f4ed05b54f8
WishMeLzzz <zhang@itsse.cn>
2021-06-09 17:45:43 +0800

    init

---
+++ config-stencil.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: "51783",
+    db: {
+        host: '',
+        port: '',
+        user: '',
+        password: '.',
+        database: '',
+        multipleStatements: true
+    },
+    TG_ROBOT_TOKEN: "",
+    md5_key:"gq4560IGRGhgv@#¥./FEFE"
+}
